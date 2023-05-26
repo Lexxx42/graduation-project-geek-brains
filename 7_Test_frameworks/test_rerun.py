f4def1097a4a5ed41ab57d6a6397d93d1da82451
Alexandr Konukhov <jesusmisha@gmail.com>
2023-05-26 10:20:20 +0300

    updated tests

--- 7_Test_frameworks/test_rerun.py
+++ 7_Test_frameworks/test_rerun.py
@@ -1,12 +1,13 @@
 from selenium.webdriver.common.by import By
-link = "http://selenium1py.pythonanywhere.com/"
+
+link = 'https://demoqa.com/'
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_guest_should_see_elements_card_on_the_main_page(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    browser.find_element(By.XPATH, '//*[@class="card mt-4 top-card"][.//h5[text()="Elements"]]')
 
 
-def test_guest_should_see_login_link_fail(browser):
+def test_guest_should_see_join_now_button_on_the_main_page(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#magic_link")
+    browser.find_element(By.CSS_SELECTOR, '#join_now')
